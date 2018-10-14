58657e1dc94bf92d4b6098a946809452674a1c7f
nitesh-me <43075695+nitesh-me@users.noreply.github.com>
2018-10-14 20:20:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var student=[{
+        "name": "nitesh",
+        "age":22,
+        "weight":60,
+        "height":180,
+        "friends":["rony","joy","illusion"],
+        "collage":{
+            "name":"SRM University",
+            "city":"chennai",
+            "pincode":845693
+        }
+    },
+    {
+        "name": "hestin",
+        "age":12,
+        "weight":60,
+        "height":150,
+        "friends":["ron","joy","illusion"],
+        "collage":{
+            "name":"SRM University",
+            "city":"chennai",
+            "pincode":845693
+        }
+    }]
+
+console.log(typeof(student));
+console.log(student);
+
